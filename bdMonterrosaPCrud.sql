bfe8f4516a45c0a016e3c1ed353bcaec01612bda
Rodrigo Monterrosa <20220377@ricaldone.edu.sv>
2024-05-17 15:44:34 -0600

    Version 1

---
+++ bdMonterrosaPCrud.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tbartista (
+    idArtista VARCHAR(50),
+    nombre VARCHAR(50),
+    nacionalidad VARCHAR(50), 
+    edad INT,
+    nombreArtisitico VARCHAR(50) 
+);
+SELECT * FROM tbartista
